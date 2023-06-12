0a74fe827ba8710a46b57cba8ae50b61a968a9e7
A-d-1-t-y-a <111745016+A-d-1-t-y-a@users.noreply.github.com>
2023-06-12 21:43:37 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ["3.bp.blogspot.com", "lh3.googleusercontent.com"],
   },
+  experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+  },
 };
 
 module.exports = nextConfig;
